17b4b5191c85f4edb5fef859df8d56f5f845e8f9
EmilGraaf <rgk510@alumni.ku.dk>
2018-10-10 00:15:49 +0200

    .lua added to toc

--- fuldildaddon.lua
+++ fuldildaddon.lua
@@ -1,11 +1,13 @@
-version = 0.1
 
+local version = 0.1
+
+--Displays version in chat window after login
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_LOGIN")
 f:SetScript("OnEvent", function(f, event)
    if event == "PLAYER_LOGIN" then
-      print("Fuld Ild" .. version .. " loaded")
+      print("|cFF00FF00Fuld Ild v" .. version .. " loaded")
 	  return
    end
-end
+ end)
  
